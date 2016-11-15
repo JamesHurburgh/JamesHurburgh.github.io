b61423970559e675d0cc2b5fadc15ca8dea2d376
James Hurburgh <onegodoneloveoneway@gmail.com>
2016-11-15 13:28:26 +1000

    Adding Table logic

--- PlaceMark/placemark.js
+++ PlaceMark/placemark.js
@@ -8,10 +8,41 @@ $(document).ready(function() {
         return list[randBetween(0, list.length)];
     }
 
+    function loadPlaceMark(script) {
+        $("#placeMark").val(script);
+    }
+
+    function readTextFile(file) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4) {
+                if (rawFile.status === 200 || rawFile.status == 0) {
+                    var allText = rawFile.responseText;
+                    alert(allText);
+                }
+            }
+        }
+        rawFile.send(null);
+    }
+
     function transform() {
         var output = $("#placeMark").val();
 
-        var tablesRegex = new RegExp(/{{{(.*?:(.|\n)*?)}}}/);
+        var tablesRegex = new RegExp(/{{([^{}]*?::[^{}]*?)}}/);
+        var tableMatch;
+        if (tableMatch = tablesRegex.exec(output)) {
+            alert("Table Match:" + tableMatch);
+            var name = tableMatch[1].split("::")[0];
+            var list = tableMatch[1].split("::")[1];
+
+            alert("TableName:" + name);
+            alert("List:" + list);
+
+
+        }
+
+
         //var placeMark = new RegExp(/{{(.|\n)*?)}}/);
         var placeMark = new RegExp(/{{([^{}]*?)}}/);
         var match;
