9d8991c170d35b8a434d52f85d64bcc376092958
James Hurburgh <onegodoneloveoneway@gmail.com>
2016-11-23 22:14:19 +1000

    Added references and extra pages

--- PlaceMark/placemark.js
+++ PlaceMark/placemark.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
 
     function randBetween(min, max) {
-        return Math.floor(Math.random() * max - min + 1) + min;
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
     function chooseRandom(list) {
         return list[randBetween(0, list.length)];
+    }
+
+    function shuffle(array){
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        while(0 !== currentIndex){
+            randomIndex = Math.floor(Math.random * currentIndex);
+            currentIndex -= 1;
+
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
     }
 
     function loadScriptList() {
@@ -115,7 +129,7 @@ $(document).ready(function() {
         scriptList.push({ "title": "2 - Getting started", "script": "# Getting started\n\nGetting started is easy. Simply fill in a sentence, then choose a word that you want to be able to randomise. For example: \n*As you step out into the street a cold breeze blows by.*\n\nPerhaps it's not a street, it's a road, or alley. First thing you need to do is wrap your word in double curly braces like this: {{street}}. Then hit the Transform button. \n*As you step out into the {{street}} a cold breeze blows by.*\n\nYou'll notice that the curly braces are gone, but not much else has changed. It's replaced the curly braces with a selection of what's inside the curly braces, but since it only has one option to choose from that's all you'll ever get. Now add some options to your list like so: \n{{street|road|alley|cul-de-sac|paved area|bustling metropolis}} \n*As you step out into the {{street|road|alley|cul-de-sac|paved area|bustling metropolis}} a cold breeze blows by.*\n\nNow when you hit the Transform button a few times, you'll notice that the output has changed each time with a random result.\n\nCongratulations! You've made your first PlaceMark script." });
         scriptList.push({ "title": "3 - Tables", "script": "# {{Tables|Lists}}\n* Define tables to look up seperately then reference them so you don't have a massive list in your text all the time. It looks like this:\nThe shadowy figure disappeared to the {{::cardinalpoint}} becomes:\n*The shadowy figure disappeared to the {{::cardinalpoint}}*\n\nWhen the table is defined like this: \n{{{cardinalpoint::: \nNorth| \nSouth| \nEast| \nWest}}}\n\n{{{cardinalpoint:::\nNorth|\nSouth|\nEast|\nWest}}}" });
         scriptList.push({ "title": "4 - Nesting", "script": "#{{Nesting|Putting PlaceMarks in other PlaceMarks}}\nPlaceMarks can be nested inside another PlaceMark. For example you might want to describe a piece of clothing, so you might want to say it's a coat or hat. Then you might want to say it's a floppy hat or a long coat. So you would do the following:\n\n*The figure is wearing a {{{{floppy |broad |}}hat|{{long |short |}}coat}} and seems to be nervous about something.*" });
-        scriptList.push({ "title": "5 - Functions", "script": "# Functions\n## Ranges\nInstead of picking from lists, you can also easily pick a random number. \nTry {{range::1-100}} to get a results like this: {{range::1-100}}, {{range::1-100}}, {{range::1-100}}\n## Comments\nComments are treated like functions in PlaceMark script, to make sure they are removed consistently from the output. Simple use the following syntax to put a comment in your script: \n{{comment::This example comment will appear in the output so you can see the syntax.}}\n{{comment::This real comment will not appear in the output.}}" });
+        scriptList.push({ "title": "# Functions\n## Ranges\nInstead of picking from lists, you can also easily pick a random number. \nTry {{range::1-100}} to get a results like this: {{range::1-100}}, {{range::1-100}}, {{range::1-100}}\n## Evaluations\nIf you'd like to do maths in your script, that's no problem. Use {{eval::1+1}} and anything in the braces will get evaluated using javascript's evaluation processes.\n## Comments\nComments are treated like functions in PlaceMark script, to make sure they are removed consistently from the output. Simple use the following syntax to put a comment in your script: \n{{comment::This example comment will appear in the output so you can see the syntax.}}\n{{comment::This real comment will not appear in the output.}}" });
         scriptList.push({ "title": "6 - Planned Features", "script": "# {{Planned Features|Stuff I want to add|Things that I think would be {{fun|cool|useful|not terrible|ground breaking|profitable|are so awesome they would unravel the very fabric of the space time continuum, and destroy the entire universe! Granted, that's a worse case scenario. The destruction might in fact be very localized, limited to merely our own galaxy.}}}}\n\n## Aliases\n* An alias is easy to define. If you want to use compasspoint as a table/list name instead of cardinal just call the table from the table like so:\n{{{compasspoint:::{{::cardinalpoint}}}}}\n\n## Versioning\n* Because if you save over your script but didn't mean to, I don't want you to lose the stuff you made.\n\n## {{Functions|Methods|Not just lists}}\n* Things like maths evaluations {{}}\n* Number ranges eg. {{range:::1-10}}\n* This leads to being able to do things like {{roll:::3d10}}\n\n## Imports\n* So you've made a PlaceMark Script that describes a commoner nicely. But now you want to create a random encounter that includes a commoner. Instead of redoing all your hard work, you want to just be able to go {{import::commoner}} don't you? Yes you do, because that would be awesome. I'll get right onto that after I've made saving and loading a thing.\n\n## {{Collaboration|Working with your {{friends|enemies|cohorts|group|buddies|crew}}|{{Using|Stealing|Borrowing}} other peoples stuff}}\n* Right now this is just hosted on a single page on github. If enough peope are interested I'll put it on it's own server and give people user accounts. Then we can:\n** Share our Scripts\n** Import other peoples scripts into your scripts (see above)\n** Favourite other peoples scripts\n** Vote on scripts\n** Comment on scripts\n** Categorise scripts to make them easier to find. " });
         scriptList.push({ "title": "Advanced: Making Images with SVG", "script": "#Making Images with SVG\nComing soon!" });
         scriptList.push({ "title": "Example - The Tavern Special", "script": "# The Tavern Special\nThe {{::price}} tavern special tonight is {{::size}} portions of {{::cookingStyle}} {{::meatType}} served with {{::sideDish}}.\nThe ingredients taste {{::quality}} though overall the meal is {{::cookingQuality}}.\n\n{{{cookingQuality:::undercooked|overcooked|burnt|too salty|too bland|too dry|too moist|just right}}}\n{{{price:::cheap|fairly priced|costly|very expensive}}}\n{{{size:::miniscule|tiny|small|normal|big|large|huge|enormous}}}\n{{{quality:::horrible|pitiful|poor|sub-standard|decent|good|quality|excellent}}}\n{{{cookingStyle:::boiled|broiled|grilled|roasted|fried|stewed}}}\n{{{sideDish:::vegetables|a salad|bread|beer|fruit|nuts|cheese|eggs}}}\n\n{{{meatType:::Alligator\n| Alpaca\n| Bear\n| Beaver\n| Beef\n| Bison\n| Buffalo\n| Camel\n| Cat\n| Chicken\n| Cougar\n| Crayfish\n| Crocodile\n| Deer\n| Dog\n| Donkey\n| Duck\n| Elk\n| Fish\n| Goose\n| Horse\n| Lamb\n| Llama\n| Lobster\n| Moose\n| Mussels\n| Mutton\n| Oysters\n| Partridge\n| Pheasant\n| Pork\n| Possum\n| Prawn\n| Quail\n| Rabbit\n| Rat\n| Red fox\n| Reindeer\n| Shrimp\n| Snake\n| Sweetbreads\n| Tripe\n| Turkey\n| Turtle\n| Veal\n| Venison\n| Wild bull\n| Wild goat\n| Wild pig\n| Mystery meat}}}" });
@@ -150,17 +164,17 @@ $(document).ready(function() {
             var fullMatch = placeMarkMatch[0];
             var innerText = placeMarkMatch[1];
             var functionCall = innerText.split("::");
-            // TODO switch here
             if (functionCall == innerText) {
                 var list = innerText.split("|");
                 if(list.constructor != Array){
                     list = [list]; // If the array only has one item it will be treated as a char array, instead of an array of one string.
                 }
                 choice = chooseRandom(list);
-            } else {
+            } else {                
                 switch (functionCall[0]) {
                     case "":
                     case "table":
+                    var list = tables[functionCall[1]];
                         if (!list) {
                             choice = "[[ERR: No table definition found for '" + functionCall[1] + "']]"
                         } else {
@@ -179,6 +193,12 @@ $(document).ready(function() {
                         }
                         choice = total;
                         break;
+                    case "shuffle":
+                        // TODO allow for custom seperator
+                        var list = functionCall[1].split("|");
+                        var seperator = ", ";
+                        choice = shuffle(list).join(seperator);
+                        break;                        
                     case "eval":
                         choice = eval(functionCall[1]);
                         break;
