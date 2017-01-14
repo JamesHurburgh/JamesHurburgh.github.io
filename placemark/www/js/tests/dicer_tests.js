e699a333c8dd25d465633572ae4caa9d14d83b40
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-01-14 23:56:32 +1000

    Qunit setup to handle tests

---
+++ placemark/www/js/tests/dicer_tests.js
@@ -0,0 +1,10 @@
+define(['jquery', 'alertify', 'store', 'mousetrap', 'showdown', 'app/parser'],
+    function($, alertify, store, mousetrap, showdown, parser) {
+
+        QUnit.module("Dicer Tests");
+
+        QUnit.test("Qunit functioning - Dicer", function(assert) {
+            assert.ok(true, "Passed!");
+        });
+
+    });
---
+++ placemark/www/js/tests/parser_tests.js
@@ -0,0 +1,10 @@
+define(['jquery', 'alertify', 'store', 'mousetrap', 'showdown', 'app/parser'],
+    function($, alertify, store, mousetrap, showdown, parser) {
+
+        QUnit.module('Parser Tests');
+
+        QUnit.test("Qunit functioning - Parser", function(assert) {
+            assert.ok(true, "Passed!");
+        });
+
+    });
